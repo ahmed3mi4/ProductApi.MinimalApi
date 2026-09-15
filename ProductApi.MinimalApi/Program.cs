90a4d3965c017a2cb594bb66136d8b28c083d881
Ahmed Emish <ahmedemish04@gmail.com>
2026-09-15 21:19:11 +0300

    MinimalApi Project

--- ProductApi.MinimalApi/Program.cs
+++ ProductApi.MinimalApi/Program.cs
@@ -1,6 +1,100 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.MinimalApi.Data;
+using ProductApi.MinimalApi.DTOs;
+using ProductApi.MinimalApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' is not configured.");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IProductService, ProductService>();
+static string? ValidateProductRequest(CreateProductRequest request)
+{
+    if(string.IsNullOrWhiteSpace(request.Name))
+        return "Product name cannot be empty.";
+    if(request.Price <= 0)
+        return "Product price must be a positive value.";
+    return null;
+}
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/products", async 
+    (CreateProductRequest request, 
+    IProductService productService,
+    CancellationToken cancellationToken) =>
+{
+    var validationError = ValidateProductRequest(request);
+    if (validationError is not null)
+    {
+        return Results.BadRequest(new
+        {
+            error = validationError
+        });
+    }
+    var response = await productService.Create(request, cancellationToken);
+    return Results.Created($"/products/{response.Id}", response);
+});
+
+app.MapGet("/products", async(IProductService productService,
+    CancellationToken cancellationToken)=>
+{ 
+    var response =  await productService.GetAll(cancellationToken);
+    return Results.Ok(response);
+}
+);
+
+app.MapGet("/products/{id:int}", async 
+    (int id,
+    IProductService productService,
+    CancellationToken cancellationToken) =>
+{
+    var response = await productService.GetById(id, cancellationToken);
+    if (response is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(response);
+}
+);
+
+app.MapPut("/products/{id:int}", async
+    (int id,
+    IProductService productService,
+    CreateProductRequest request,
+    CancellationToken cancellationToken) =>
+{
+    var validationError = ValidateProductRequest(request);
+
+    if (validationError is not null)
+    {
+        return Results.BadRequest(new
+        {
+            error = validationError
+        });
+    }
+    var response = await productService.Update(id, request, cancellationToken);
+    if (response is null)
+        return Results.NotFound();
+    return Results.Ok(response);
+}
+);
+
+app.MapDelete("/products/{id:int}", async
+    (int id,
+    IProductService productService,
+    CancellationToken cancallationToken)=>
+{
+    var response = await productService.Delete(id, cancallationToken);
+    if (response)
+        return Results.NoContent();
+    return Results.NotFound();
+}
+);
 
 app.Run();
---
+++ ProductApi.MinimalApi/Services/ProductService.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.MinimalApi.Data;
+using ProductApi.MinimalApi.DTOs;
+using ProductApi.MinimalApi.Models;
+
+namespace ProductApi.MinimalApi.Services;
+
+public class ProductService : IProductService
+{
+    private readonly AppDbContext _context;
+    private readonly ILogger<ProductService> _logger;
+    public ProductService(AppDbContext context, ILogger<ProductService> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+    public async Task<ProductResponse> Create(CreateProductRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var product = new Product
+            {
+                Name = request.Name,
+                Price = request.Price,
+                CreatedAtUtc = DateTime.UtcNow
+            };
+
+            _context.Products.Add(product);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            _logger.LogInformation(
+                    "Product created successfully. ProductId: {ProductId}",
+                    product.Id);
+
+            return new ProductResponse
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                CreatedAtUtc = product.CreatedAtUtc
+            };
+
+        }
+        catch (Exception ex) 
+        {
+            _logger.LogError(
+           ex,
+           "Failed to create product. Name: {ProductName}",
+           request.Name);
+
+            throw;
+        }
+    }
+    public async Task<IEnumerable<ProductResponse>> GetAll(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var products = await _context.Products
+                .AsNoTracking()
+                .Select(p => new ProductResponse
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    CreatedAtUtc = p.CreatedAtUtc
+                }).ToListAsync(cancellationToken);
+
+            _logger.LogInformation(
+                 "Retrieved {ProductCount} products",
+                            products.Count);
+            return products;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                ex,
+                "Failed to retrieve products");
+            throw;
+        }
+    }
+
+    public async Task<ProductResponse?> GetById(int id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var product = await _context.Products
+                 .AsNoTracking()
+                 .Select(p => new ProductResponse
+                 {
+                     Id = p.Id,
+                     Name = p.Name,
+                     Price = p.Price,
+                     CreatedAtUtc = p.CreatedAtUtc
+                 }).FirstOrDefaultAsync((p => p.Id == id), cancellationToken);
+
+            if (product is null)
+            {
+                _logger.LogWarning(
+                    "Product not found. ProductId: {ProductId}",
+                    id);
+
+                return null;
+            }
+
+            _logger.LogInformation(
+                "Product retrieved successfully. ProductId: {ProductId}",
+                id);
+
+            return product;
+        }
+        catch(Exception ex)
+            {
+                _logger.LogError(ex, "Failed to retrieve product. ProductId: {ProductId}", id);
+                throw;
+            }
+    }
+
+    public async Task<ProductResponse?> Update(int id, CreateProductRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+
+            var effectedRows = await _context.Products
+             .Where(p => p.Id == id)
+             .ExecuteUpdateAsync(p => p
+                 .SetProperty(p => p.Name, request.Name)
+                 .SetProperty(p => p.Price, request.Price), cancellationToken);
+            if (effectedRows == 0)
+            {
+                _logger.LogWarning(
+           "Product not found for update. ProductId: {ProductId}",
+                   id);
+                return null;
+            }
+            _logger.LogInformation(
+        "Product updated successfully. ProductId: {ProductId}",
+                id);
+            return await GetById(id, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to update product. ProductId: {ProductId}", id);
+            throw;
+        }
+    }
+    public async Task<bool> Delete(int id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var effectedRows = await _context.Products
+                .Where(p => p.Id == id)
+                .ExecuteDeleteAsync(cancellationToken);
+            if (effectedRows == 0)
+            {
+                _logger.LogWarning(
+                    "Product not found for deletion. ProductId: {ProductId}",
+                    id);
+
+                return false;
+            }
+
+            _logger.LogInformation(
+                "Product deleted successfully. ProductId: {ProductId}",
+                id);
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+           ex,
+           "Failed to delete product. ProductId: {ProductId}",
+           id);
+
+            throw;
+        }
+    }
+
+}
+
+   
